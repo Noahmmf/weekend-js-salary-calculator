8c7a134f71a13bbc0199d4ec553333a7dc55dbd4
Noahmmf <noahfuglestad@gmail.com>
2024-01-05 15:36:08 -0600

    started project

---
+++ script.js
@@ -0,0 +1,13 @@
+onReady();
+function onReady(){
+    console.log('Working');
+}
+
+let employees = [];
+
+//function adds employee to table
+function addEmployees(event){
+    preventDefault();
+
+
+}
