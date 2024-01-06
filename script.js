05ff3e4a8706b35e039ea3e88307c6129f1b4d01
Noahmmf <noahfuglestad@gmail.com>
2024-01-05 18:58:48 -0600

    form function working correctly

--- script.js
+++ script.js
@@ -3,13 +3,44 @@ function onReady(){
     console.log('Working');
 }
 
-let employees = [];
+let allEmployees = [];
+
+//variables for creating objects
+let firstName = document.getElementById("f-name");
+let lastName = document.getElementById("l-name");
+let empId = document.getElementById("emp-id");
+let jobTitle = document.getElementById("job-title");
+let annualSalary = document.getElementById("salary");
 
 //function adds employee to table
 function addEmployees(event){
     //prevent default behaivor
     event.preventDefault();
+    //create an employee Object
+    let employeeObject = { 
+    Name:firstName.value + " " + lastName.value,
+    empId: empId.value,
+    annualSalary: annualSalary.value}
+    
+    // when submitting info add inofrmation to global variable array employees.
+    allEmployees.push(employeeObject);
 
-    let onSubmit = document.getElementById('')
+    let tableRow = document.getElementById("tableOfEmployees");
 
-}
+    tableRow.innerHTML +=`
+    <tr class = "tableRow">
+        <td>${firstName.value + " " + lastName.value}</td>
+        <td>${empId.value}</td>
+        <td>${jobTitle.value}</td>
+        <td>${annualSalary.value}</td>
+    </tr>
+    ` 
+    //clear input forms
+    firstName.value = '';
+    lastName.value = '';
+    empId = '';
+    jobTitle.value = '';
+    annualSalary.value = '';
+
+    console.log(allEmployees);
+}// end function addEmployees
